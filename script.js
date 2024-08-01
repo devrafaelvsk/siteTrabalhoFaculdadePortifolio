60a672e7352a9aac77413396dc70563377095ff2
Rafael Vesoloski <134641899+devrafaelvsk@users.noreply.github.com>
2024-08-01 15:46:51 -0300

    Atualizações

---
+++ script.js
@@ -0,0 +1,14 @@
+// Animação suave para links internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 80,
+            behavior: 'smooth'
+        });
+    });
+});
